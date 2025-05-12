1fa7cf42306c30075cbc42ba2340d1f58cd1960e
Jayshree Sakhare <sakharejayshree37@gmail.com>
2025-05-12 23:29:20 +0530

    String_01

---
+++ src/sampleProgram/String_01.java
@@ -0,0 +1,16 @@
+package sampleProgram;
+
+public class String_01 {
+
+	public static void main(String[] args) {
+	        String original = "hello";
+	        String reversed = "";
+
+	        for (int i = original.length() - 1; i >= 0; i--) {
+	            reversed += original.charAt(i);
+	        }
+
+	        System.out.println("Reversed string: " + reversed);
+	    }
+	}
+
